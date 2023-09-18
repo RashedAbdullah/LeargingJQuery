821d32a488db7a18473d13be654ad6f74ab85947
Rashed Abdullah <maarashed40@gamil.com>
2023-09-18 23:19:24 +0600

    somthing added

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 console.log(`'JQUERY' is here`);
+
+// $(document).ready(function(){
+//     $(".p1").click(function(){
+//       $(this).hide();
+//     });
+// });
+
+// $(document).ready(function () {
+//     $(".p3").mouseover(function () { 
+//         $(this).hide();
+//     });
+// });
+
+
+$(document).ready(function () {
+    $(`.forNext`).slick({
+        autoplay: true, // (for autoplay next or previos)
+        autoplaySpeed: 1000,
+        arrows: true, //(default true, we can turn as false);
+        dots: true,
+        prevArrow: `<button type="button" class="slick-prev"><</button>`,
+        nextArrow: `<button type="button" class="slick-next">></button>`
+    })
+
+
+    $(`.forNext2`).slick({
+        autoplay: false, // (for auto next and previos)
+        autoplaySpeed: 1000,
+        arrows: true, //(default true, we can turn as false);
+        dots: true,
+        prevArrow: `<button type="button" class="slick-prev"><</button>`,
+        nextArrow: `<button type="button" class="slick-next">></button>`,
+        slidesToShow: 2,
+    })
+
+
+});
